75ee8228128624bb892fbf8a0d91aa632d5579e2
ArtZ23 <zzartur77@gmail.com>
2023-05-06 19:12:24 +0400

    add homework files

---
+++ Homework4IT5/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Homework4IT5
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Artur Zakharyan");
+			Console.BackgroundColor = ConsoleColor.Blue;
+			Console.WriteLine(Console.ReadLine());
+		}
+	}
+}
